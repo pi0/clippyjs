ab9474107c27568b5f31370a561ea1ba50776e70
Pooya Parsa <pooya@pi0.ir>
2017-06-10 15:33:12 +0430

    Update Demo

---
+++ demo/demo.js
@@ -0,0 +1,50 @@
+var availableAgents = ['Bonzi', 'Clippy', 'F1', 'Genie', 'Genius', 'Links', 'Merlin', 'Peedy', 'Rocky', 'Rover']
+
+var talks = [
+    'How can i help you?',
+    'Nice day!',
+    'Glad to meet you.',
+    'At your service',
+    'Helloo'
+]
+
+const randPos = () => .2 + Math.random() * .6
+
+function nextAgent () {
+    let agentName = availableAgents.pop()
+    if (!agentName) return;
+
+    clippy.load(agentName, agent => {
+        window[agentName] = agent
+
+        const move = () => {
+            agent.moveTo($(document).width() * randPos(), $(document).height() * randPos())
+        }
+
+        move()
+
+        agent.show();
+
+        // Speak on click and start
+        const speak = () => {
+            agent.speak('I am ' + agentName + ', ' + talks[~~(Math.random() * talks.length)])
+            agent.animate()
+        }
+        $(agent._el).click(() => speak())
+        speak()
+
+        // Animate randomly
+        setInterval(() => {
+            agent.animate()
+        }, 3000 + (Math.random() * 4000))
+
+        // Move randomly
+        setInterval(() => {
+            move()
+        }, 3000 + (Math.random() * 4000))
+
+        setTimeout(nextAgent, 2000)
+    }, undefined, '../assets/agents/');
+}
+
+nextAgent()
